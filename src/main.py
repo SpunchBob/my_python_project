a4804366ef2a5f17dc069f7a18bcb947e569641d
SpunchBob <aleksandrprokudin2007@gmail.com>
2026-03-04 16:35:47 -0500

    feat(parser): added parse_list_to_json method

--- src/main.py
+++ src/main.py
@@ -7,5 +7,13 @@ if __name__ == "__main__":
         "name" : "str",
         "course" : "int"
         }
-    json_str = parser.parse_dict_to_json(dict_)
-    print(f"{json_str}")
+    list_ = [
+        "aleks",
+        "ivan",
+        "max"
+        ]
+    json_str_dict = parser.parse_dict_to_json(dict_)
+    json_str_list = parser.parse_list_to_json(list_)
+    print(f"{json_str_dict}")
+    print(f"{json_str_list}")
+
--- src/my_package/parser.py
+++ src/my_package/parser.py
@@ -8,3 +8,10 @@ class Parser:
             result += f"    {key} : {value}\n"
         result += '}'
         return result
+
+    def parse_list_to_json(self, list_: list) -> str:
+        result = "{\n"
+        for item in list_:
+            result += f"    {item},\n"
+        result += "}"
+        return result 
